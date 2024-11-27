ce041b001fc524b3c31295cd848d160bc816b584
OKenedy <ayomideademide72@gmail.com>
2024-11-27 16:17:55 +0100

    (seventh)

---
+++ week-6/Practice_7/target/debug/Practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\Ifeoluwa\ Oloyede\Documents\I.oloyedeCSC101\week-6\Practice_7\target\debug\Practice_7.exe: C:\Users\Ifeoluwa\ Oloyede\Documents\I.oloyedeCSC101\week-6\Practice_7\src\main.rs
---
+++ week-6/Practice_7/target/debug/deps/Practice_7.d
@@ -0,0 +1,5 @@
+C:\Users\Ifeoluwa Oloyede\Documents\I.oloyedeCSC101\week-6\Practice_7\target\debug\deps\Practice_7.exe: src/main.rs
+
+C:\Users\Ifeoluwa Oloyede\Documents\I.oloyedeCSC101\week-6\Practice_7\target\debug\deps\Practice_7.d: src/main.rs
+
+src/main.rs:
