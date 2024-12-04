95351a454e07ace39a3648afb8020d1af21f17fb
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:53:07 +0100

    (fifth practice)

--- week-8/practice_5/src/main.rs
+++ week-8/practice_5/src/main.rs
@@ -1,6 +1,33 @@
 fn main() {
-    let vec1 = vec![1, 2, 3];
-    let vec2 = vec![4, 5, 6];
-    let vec3: Vec<_> = vec1.iter().chain(vec2.iter()).cloned().collect();
-    println!("Combined Vector: {:?}", vec3);
+    // Create an empty vector "City"
+    let mut city: Vec<String> = Vec::new();
+
+    // Print City Vector
+    println!("The City vector has element {}", city.len());
+
+    // Push new elements into
+    let mut input1 = String::new();
+    println!("How many Cities do you want to enter");
+    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
+
+    let city_num: usize = input1.trim().parse().expect("Invalid input");
+
+    for count in 0..city_num {
+        let mut input2 = String::new();
+        println!("Enter City {}", count + 1);
+        std::io::stdin().read_line(&mut input2).expect("Failed to read input");
+
+        let new_city: String = input2.trim().parse().expect("Invalid input");
+        city.push(new_city);
+    }
+
+    println!("Your preferred cities are:");
+
+    let mut count = 1;
+    // loop to iterate elements in vector
+    for i in city {
+        // iterating through i on the vector
+        println!("{} {}", count, i);
+        count += 1;
+    }
 }
