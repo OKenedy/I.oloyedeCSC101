75be0704ac077ac5cb51b96b08e2f5bd7b1f8332
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:21:10 +0100

    (third practice)

--- week-8/practice_3/src/main.rs
+++ week-8/practice_3/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let vec = vec![10, 20, 30, 40];
-    if let Some(value) = vec.get(2) {
-        println!("Element at index 2: {}", value);
-    } else {
-        println!("No element found!");
+    let vec = vec![1, 2, 3, 4, 5];
+    for val in vec.iter() {
+        println!("Value: {}", val);
     }
 }
