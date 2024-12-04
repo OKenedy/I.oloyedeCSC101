8e00a5eea8dc7bacd2cf202a3344dd1e4f1787ec
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:57:29 +0100

    (eigth practice)

--- week-8/practice_8/src/main.rs
+++ week-8/practice_8/src/main.rs
@@ -1,8 +1,12 @@
-fn print_tuple(data: (i32, f64, &str)) {
-    println!("Tuple elements: {:?}", data);
-}
-
 fn main() {
-    let tuple = (10, 20.5, "Rust");
-    print_tuple(tuple);
+    // initialize a mutable tuple
+    let mut mountain_heights = ("Everest", 8848, "Fishtail", 6993);
+
+    println!("Original tuple = {:?}", mountain_heights);
+
+    // change 3rd and 4th element of a mutable tuple
+    mountain_heights.2 = "Lhotse";
+    mountain_heights.3 = 8516;
+
+    println!("Changed tuple = {:?}", mountain_heights);
 }
