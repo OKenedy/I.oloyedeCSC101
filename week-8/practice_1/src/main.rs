d38d83bf36b6b3a9160043b0e8a07884b6904867
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:46:00 +0100

    (first practice)

--- week-8/practice_1/src/main.rs
+++ week-8/practice_1/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
-    let vec1 = vec![1, 2, 3, 4, 5];
-    println!("Vector: {:?}", vec1);
+    // Using Vec::new()
+    let v: Vec<i64> = Vec::new();
+    // printing the size of vector
+    println!("\nThe length of Vec::new is: {}", v.len());
+
+    // Using macro
+    let v = vec!["Grace", "Effiong", "Basil", "Kareem", "Susan"];
+    // printing the size of vector
+    println!("\nThe length of vec macro is: {}", v.len());
 }
