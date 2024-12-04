2616d6316b7abaa382ab21fbd36ec52efcaca76c
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:50:41 +0100

    (third practice)

--- week-8/practice_3/src/main.rs
+++ week-8/practice_3/src/main.rs
@@ -1,6 +1,18 @@
+fn value(n: Option<&char>) {
+    println!("Element of vector {:?}", n);
+}
+
 fn main() {
-    let vec = vec![1, 2, 3, 4, 5];
-    for val in vec.iter() {
-        println!("Value: {}", val);
-    }
+    let v = vec!['R', 'U', 'S', 'T', 'A', 'C', 'I', 'A', 'N'];
+
+    let mut input1 = String::new();
+    println!("Enter an index value btw (0 - 8):");
+    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
+
+    // index is the non-negative value which is smaller than the size of the vector
+    let index: usize = input1.trim().parse().expect("Invalid input");
+
+    // getting value at given index value
+    let ch: Option<&char> = v.get(index);
+    value(ch);
 }
