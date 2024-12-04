8e453afcedf1a27356f15433d06b5489892bec8b
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:33:49 +0100

    (tenth practice)

---
+++ week-8/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\Ifeoluwa\ Oloyede\Documents\I.oloyedeCSC101\week-8\practice_10\target\debug\practice_10.exe: C:\Users\Ifeoluwa\ Oloyede\Documents\I.oloyedeCSC101\week-8\practice_10\src\main.rs
