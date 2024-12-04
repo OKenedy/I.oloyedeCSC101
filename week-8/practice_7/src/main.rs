da17bb0eed2276a7eefbc039ee7d00289592f152
OKenedy <ayomideademide72@gmail.com>
2024-12-04 15:25:49 +0100

    (seventh practice)

---
+++ week-8/practice_7/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut tuple = (1, 2, 3);
+    tuple.1 = 10;
+    println!("Updated Tuple: {:?}", tuple);
+}
---
+++ week-8/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\Ifeoluwa\ Oloyede\Documents\I.oloyedeCSC101\week-8\practice_7\target\debug\practice_7.exe: C:\Users\Ifeoluwa\ Oloyede\Documents\I.oloyedeCSC101\week-8\practice_7\src\main.rs
