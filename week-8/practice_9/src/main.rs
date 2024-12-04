1461963d29769756452401d1bcc46bf27d9ddc42
OKenedy <ayomideademide72@gmail.com>
2024-12-04 16:01:38 +0100

    (ninth practice)

--- week-8/practice_9/src/main.rs
+++ week-8/practice_9/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let tuple = (10, 20.5, "Rust");
-    let (a, b, c) = tuple;
-    println!("Destructured Tuple: {}, {}, {}", a, b, c);
+    let b: (i32, bool, f64) = (110, true, 10.9);
+    print(b);
+}
+
+// pass the tuple as a parameter
+fn print(x: (i32, bool, f64)) {
+    println!("Inside print method");
+    println!("{:?}",x);
 }
