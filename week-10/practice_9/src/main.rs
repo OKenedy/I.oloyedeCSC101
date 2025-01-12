7588ff3537b355ede0716b1ee5c58215c177e446
OKenedy <ayomideademide72@gmail.com>
2025-01-12 09:35:05 +0100

    (ninth)

---
+++ week-10/practice_9/src/main.rs
@@ -0,0 +1,24 @@
+// Define dimensions of a rectangle
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// Logic to calculate area of a rectangle
+impl Rectangle {
+    fn area(&self) -> u32 {
+        // Use the self operator to fetch the value of a field via the self keyword
+        self.width * self.height
+    }
+}
+
+fn main() {
+    // Instantiate the structure
+    let small = Rectangle {
+        width: 10,
+        height: 20,
+    };
+
+    // Print the rectangle's area
+    println!("width is {} \n height is {} \n area of Rectangle is {}", small.width, small.height, small.area());
+}
